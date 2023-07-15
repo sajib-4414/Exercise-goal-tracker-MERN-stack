0bcdcbb52c33e12b67e26853e7f06e0af608ebb0
Shamsul Arefin <shamsul.arefin.sajib@gmail.com>
2023-07-15 07:24:44 +0000

    call to login works

--- backend/server.js
+++ backend/server.js
@@ -18,17 +18,17 @@ app.use(express.urlencoded({extended:false}))
 app.use('/api/goals', require('./routes/goalRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
-//serve frontend
-if(process.env.NODE_ENV == "production"){
-    //for production, we have to build the application into html build, there will be an index html entry
-    //file for the production build. That build is only created when you run npm run build
-    //with dev server running,that build is not created, once the build is created, then...
-    //first set the static/build folder, that is the folder, react will build assets
-    app.use(express.static(path.join(__dirname,"../frontend/build")))
-    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname,"../","frontend","build","index.html")))
-} else{
-    app.get("/", (req,res) => res.send("Please set to production"))
-}
+// //serve frontend
+// if(process.env.NODE_ENV == "production"){
+//     //for production, we have to build the application into html build, there will be an index html entry
+//     //file for the production build. That build is only created when you run npm run build
+//     //with dev server running,that build is not created, once the build is created, then...
+//     //first set the static/build folder, that is the folder, react will build assets
+//     app.use(express.static(path.join(__dirname,"../frontend/build")))
+//     app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname,"../","frontend","build","index.html")))
+// } else{
+//     app.get("/", (req,res) => res.send("Please set to production"))
+// }
 app.use(errorHandler)
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`))
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-
-const API_URL = 'http://localhost:5000/api/users/'
+const base_url = process.env.REACT_APP_API_URI
+const API_URL = base_url + '/api/users/'
 
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData)
